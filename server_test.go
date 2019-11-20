072decf84245a53959cf9f07bce0eaed806e9656
Saimon <semyuon@gmail.com>
2019-11-20 12:52:15 +0300

    chore: add base test core

---
+++ server_test.go
@@ -0,0 +1,50 @@
+package socketio
+
+import (
+	"testing"
+)
+
+// type TestCase struct {
+// }
+
+// type ErrorTestCase struct {
+// 	Message string
+// }
+
+func TestDefaulServer(t *testing.T) {
+	// server, err := NewServer(nil)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// httpServer := httptest.NewServer(server)
+	// defer httpServer.Close()
+}
+func TestGinServer(t *testing.T) {
+	// server, err := NewServer(nil)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// httpServer := httptest.NewServer(server)
+	// defer httpServer.Close()
+}
+
+func TestGorillaServer(t *testing.T) {
+	// server, err := NewServer(nil)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// httpServer := httptest.NewServer(server)
+	// defer httpServer.Close()
+}
+
+// func BenchmarkDefaultServer(b *testing.B) {
+
+// }
+
+// func BenchmarkGorillaServer(b *testing.B) {
+
+// }
+
+// func BenchmarkGinServer(b *testing.B) {
+
+// }
