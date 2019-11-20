60e1f7dc602b75c145fce979584f1a29f9c46595
Saimon <semyuon@gmail.com>
2019-11-20 12:04:47 +0300

    ref: optimize source code

--- broadcast.go
+++ broadcast.go
@@ -2,49 +2,53 @@ package socketio
 
 import "sync"
 
+// BroadcastConn is overall intarface same a intarface about connection Conn in go-socket.io
+type BroadcastConn interface {
+	ID() string                        // ID returns session id by connect
+	Emit(msg string, v ...interface{}) // Emit message to client
+}
+
 // Broadcast is the adaptor to handle broadcasts & rooms for socket.io server API
 type Broadcast interface {
-	Join(room string, connection Conn)            // Join causes the connection to join a room
-	Leave(room string, connection Conn)           // Leave causes the connection to leave a room
-	LeaveAll(connection Conn)                     // LeaveAll causes given connection to leave all rooms
+	Join(room string, connection BroadcastConn)   // Join causes the connection to join a room
+	Leave(room string, connection BroadcastConn)  // Leave causes the connection to leave a room
+	LeaveAll(connection BroadcastConn)            // LeaveAll causes given connection to leave all rooms
 	Clear(room string)                            // Clear causes removal of all connections from the room
 	Send(room, event string, args ...interface{}) // Send will send an event with args to the room
 	SendAll(event string, args ...interface{})    // SendAll will send an event with args to all the rooms
 	Len(room string) int                          // Len gives number of connections in the room
-	Rooms(connection Conn) []string               // Gives list of all the rooms if no connection given, else list of all the rooms the connection joined
+	Rooms(connection BroadcastConn) []string      // Gives list of all the rooms if no connection given, else list of all the rooms the connection joined
 }
 
 // broadcast gives Join, Leave & BroadcastTO server API support to socket.io along with room management
 type broadcast struct {
-	rooms map[string]map[string]Conn // map of rooms where each room contains a map of connection id to connections in that room
-	lock  sync.RWMutex               // access lock for rooms
+	lock  sync.RWMutex                        // access lock for rooms
+	rooms map[string]map[string]BroadcastConn // map of rooms where each room contains a map of connection id to connections in that room
 }
 
 // NewBroadcast creates a new broadcast adapter
 func NewBroadcast() Broadcast {
-	return &broadcast{rooms: make(map[string]map[string]Conn)}
+	return &broadcast{rooms: make(map[string]map[string]BroadcastConn)}
 }
 
 // Join joins the given connection to the broadcast room
-func (broadcast *broadcast) Join(room string, connection Conn) {
-	// get write lock
+func (broadcast *broadcast) Join(room string, connection BroadcastConn) {
 	broadcast.lock.Lock()
-	defer broadcast.lock.Unlock()
 
 	// check if room already has connection mappings, if not then create one
 	if _, ok := broadcast.rooms[room]; !ok {
-		broadcast.rooms[room] = make(map[string]Conn)
+		broadcast.rooms[room] = make(map[string]BroadcastConn)
 	}
 
 	// add the connection to the rooms connection map
 	broadcast.rooms[room][connection.ID()] = connection
+
+	broadcast.lock.Unlock()
 }
 
 // Leave leaves the given connection from given room (if exist)
-func (broadcast *broadcast) Leave(room string, connection Conn) {
-	// get write lock
+func (broadcast *broadcast) Leave(room string, connection BroadcastConn) {
 	broadcast.lock.Lock()
-	defer broadcast.lock.Unlock()
 
 	// check if rooms connection
 	if connections, ok := broadcast.rooms[room]; ok {
@@ -56,13 +60,13 @@ func (broadcast *broadcast) Leave(room string, connection Conn) {
 			delete(broadcast.rooms, room)
 		}
 	}
+
+	broadcast.lock.Unlock()
 }
 
 // LeaveAll leaves the given connection from all rooms
-func (broadcast *broadcast) LeaveAll(connection Conn) {
-	// get write lock
+func (broadcast *broadcast) LeaveAll(connection BroadcastConn) {
 	broadcast.lock.Lock()
-	defer broadcast.lock.Unlock()
 
 	// iterate through each room
 	for room, connections := range broadcast.rooms {
@@ -74,36 +78,33 @@ func (broadcast *broadcast) LeaveAll(connection Conn) {
 			delete(broadcast.rooms, room)
 		}
 	}
+	broadcast.lock.Unlock()
 }
 
 // Clear clears the room
 func (broadcast *broadcast) Clear(room string) {
-	// get write lock
 	broadcast.lock.Lock()
-	defer broadcast.lock.Unlock()
 
-	// delete the room
 	delete(broadcast.rooms, room)
+
+	broadcast.lock.Unlock()
 }
 
 // Send sends given event & args to all the connections in the specified room
 func (broadcast *broadcast) Send(room, event string, args ...interface{}) {
-	// get a read lock
 	broadcast.lock.RLock()
-	defer broadcast.lock.RUnlock()
 
 	// iterate through each connection in the room
 	for _, connection := range broadcast.rooms[room] {
 		// emit the event to the connection
 		connection.Emit(event, args...)
 	}
+	broadcast.lock.RUnlock()
 }
 
 // SendAll sends given event & args to all the connections to all the rooms
 func (broadcast *broadcast) SendAll(event string, args ...interface{}) {
-	// get a read lock
 	broadcast.lock.RLock()
-	defer broadcast.lock.RUnlock()
 
 	// iterate through each room
 	for _, connections := range broadcast.rooms {
@@ -113,37 +114,34 @@ func (broadcast *broadcast) SendAll(event string, args ...interface{}) {
 			connection.Emit(event, args...)
 		}
 	}
+	broadcast.lock.RUnlock()
 }
 
 // Len gives number of connections in the room
 func (broadcast *broadcast) Len(room string) int {
 	broadcast.lock.RLock()
-	defer broadcast.lock.RUnlock()
-
-	return len(broadcast.rooms[room])
+	conn2Room := len(broadcast.rooms[room])
+	broadcast.lock.RUnlock()
+	return conn2Room
 }
 
 // Rooms gives the list of all the rooms available for broadcast in case of
 // no connection is given, in case of a connection is given, it gives
 // list of all the rooms the connection is joined to
-func (broadcast *broadcast) Rooms(connection Conn) []string {
+func (broadcast *broadcast) Rooms(connection BroadcastConn) []string {
 	broadcast.lock.RLock()
-	defer broadcast.lock.RUnlock()
+
+	// new list of all the room names
+	var rooms []string
 
 	if connection == nil {
-		// create a new list of all the room names
-		rooms := make([]string, 0)
-
+		rooms = make([]string, 0, len(broadcast.rooms))
 		// iterate through the rooms map and add the room name to the above list
 		for room := range broadcast.rooms {
 			rooms = append(rooms, room)
 		}
-
-		return rooms
 	} else {
-		// create a new list of all the room names the connection is joined to
-		rooms := make([]string, 0)
-
+		rooms = make([]string, 0)
 		// iterate through the rooms map and add the room name to the above list
 		for room, connections := range broadcast.rooms {
 			// check if the connection is joined to the room
@@ -152,7 +150,8 @@ func (broadcast *broadcast) Rooms(connection Conn) []string {
 				rooms = append(rooms, room)
 			}
 		}
-
-		return rooms
 	}
+	broadcast.lock.RUnlock()
+
+	return rooms
 }
